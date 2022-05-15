e47ba4ec38f4f909b7dc1c26a04b27601e1c5319
Ziaul Chowdhury <romel99@gmail.com>
2022-05-16 00:03:16 +0200

    Implemented transfer learning (vgg16 & resnet50 architectures) to classify pneumonia

---
+++ project/pneumonia_model_transfer_learning.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 16:44:47 2022
+
+@author: ziaul
+"""
+
+from timeit import default_timer as timer
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim, cuda
+
+from torchvision import transforms, datasets, models
+from torchvision.datasets import ImageFolder
+
+from torch.utils.data.dataloader import DataLoader
+from torchvision.utils import make_grid
+# from torchmetrics.functional import f1_score
+
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, recall_score, accuracy_score, f1_score
+
+def load_pneumonia_dataset(data_dir, batch_size=128):
+    
+    image_transforms = {
+        # Train uses data augmentation
+        'train': transforms.Compose([
+                transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+                transforms.RandomRotation(degrees=15),
+                transforms.ColorJitter(),
+                transforms.RandomHorizontalFlip(),
+                transforms.CenterCrop(size=224),  # Image net standards
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406],
+                                     [0.229, 0.224, 0.225])  # Imagenet standards
+            ]),
+        # Validation does not use augmentation
+        'val': transforms.Compose([
+                transforms.Resize(size=256),
+                transforms.CenterCrop(size=224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        # Test does not use augmentation
+        'test': transforms.Compose([
+                transforms.Resize(size=256),
+                transforms.CenterCrop(size=224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+    }
+    
+    data = {
+        'train': datasets.ImageFolder(root=data_dir + 'train', transform=image_transforms['train']),
+        'val': datasets.ImageFolder(root=data_dir + 'val', transform=image_transforms['val']),
+        'test': datasets.ImageFolder(root=data_dir + 'test', transform=image_transforms['test'])
+    }
+    
+    # Dataloader iterators
+    dataloaders = {
+        'train': DataLoader(data['train'], batch_size=batch_size, shuffle=True),
+        'val': DataLoader(data['val'], batch_size=batch_size, shuffle=True),
+        'test': DataLoader(data['test'], batch_size=batch_size, shuffle=True)
+    }
+    
+    return dataloaders
+
+def get_pretrained_model(model_name, n_classes=2, train_on_gpu=True):
+    """ Retrieve a pre-trained model from torchvision """
+
+    if model_name == 'vgg16':
+        model = models.vgg16(pretrained=True)
+
+        # Freeze early layers
+        for param in model.parameters():
+            param.requires_grad = False
+        n_inputs = model.classifier[6].in_features
+
+        # Add on classifier
+        model.classifier[6] = nn.Sequential(
+            nn.Linear(n_inputs, 256), nn.ReLU(), nn.Dropout(0.2),
+            nn.Linear(256, n_classes), nn.LogSoftmax(dim=1))
+
+    elif model_name == 'resnet50':
+        model = models.resnet50(pretrained=True)
+
+        for param in model.parameters():
+            param.requires_grad = False
+
+        n_inputs = model.fc.in_features
+        model.fc = nn.Sequential(
+            nn.Linear(n_inputs, 256), nn.ReLU(), nn.Dropout(0.2),
+            nn.Linear(256, n_classes), nn.LogSoftmax(dim=1))
+
+    # Move to gpu and parallelize
+    if train_on_gpu:
+        model = model.to('cuda')
+
+    return model
+
+
+def train(model, criterion, optimizer, train_loader, valid_loader, save_file_name, max_epochs_stop=3, n_epochs=20, print_every=2, train_on_gpu=True):
+
+    # Early stopping intialization
+    epochs_no_improve = 0
+    valid_loss_min = np.Inf
+
+    valid_max_acc = 0
+    history = []
+
+    # Number of epochs already trained (if using loaded in model weights)
+    try:
+        print(f'Model has been trained for: {model.epochs} epochs.\n')
+    except:
+        model.epochs = 0
+        print('Starting Training from Scratch.\n')
+
+    overall_start = timer()
+
+    # Main loop
+    for epoch in range(n_epochs):
+
+        # keep track of training and validation loss each epoch
+        train_loss = 0.0
+        valid_loss = 0.0
+
+        train_acc = 0
+        valid_acc = 0
+
+        # Set to training
+        model.train()
+        start = timer()
+
+        # Training loop
+        for ii, (data, target) in enumerate(train_loader):
+            # Tensors to gpu
+            if train_on_gpu:
+                data, target = data.cuda(), target.cuda()
+
+            # Clear gradients
+            optimizer.zero_grad()
+            # Predicted outputs are log probabilities
+            output = model(data)
+
+            # Loss and backpropagation of gradients
+            loss = criterion(output, target)
+            loss.backward()
+
+            # Update the parameters
+            optimizer.step()
+
+            # Track train loss by multiplying average loss by number of examples in batch
+            train_loss += loss.item() * data.size(0)
+
+            # Calculate accuracy by finding max log probability
+            _, pred = torch.max(output, dim=1)
+            correct_tensor = pred.eq(target.data.view_as(pred))
+            correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+                
+            # Need to convert correct tensor from int to float to average
+            accuracy = torch.mean(correct_tensor.type(torch.FloatTensor))
+            # Multiply average accuracy times the number of examples in batch
+            train_acc += accuracy.item() * data.size(0)
+            
+            # Track training progress
+            print(f'Epoch: {epoch}\t{100 * (ii + 1) / len(train_loader):.2f}% complete. {timer() - start:.2f} seconds elapsed in epoch.', end='\r')
+
+        # After training loops ends, start validation
+        else:
+            model.epochs += 1
+
+            # Don't need to keep track of gradients
+            with torch.no_grad():
+                # Set to evaluation mode
+                model.eval()
+
+                # Validation loop
+                for data, target in valid_loader:
+                    # Tensors to gpu
+                    if train_on_gpu:
+                        data, target = data.cuda(), target.cuda()
+
+                    # Forward pass
+                    output = model(data)
+
+                    # Validation loss
+                    loss = criterion(output, target)
+                    # Multiply average loss times the number of examples in batch
+                    valid_loss += loss.item() * data.size(0)
+
+                    # Calculate validation accuracy
+                    _, pred = torch.max(output, dim=1)
+                    correct_tensor = pred.eq(target.data.view_as(pred))
+                    accuracy = torch.mean(
+                        correct_tensor.type(torch.FloatTensor))
+                    # Multiply average accuracy times the number of examples
+                    valid_acc += accuracy.item() * data.size(0)
+
+                # Calculate average losses
+                train_loss = train_loss / len(train_loader.dataset)
+                valid_loss = valid_loss / len(valid_loader.dataset)
+
+                # Calculate average accuracy
+                train_acc = train_acc / len(train_loader.dataset)
+                valid_acc = valid_acc / len(valid_loader.dataset)
+
+                history.append([train_loss, valid_loss, train_acc, valid_acc])
+
+                # Print training and validation results
+                if (epoch + 1) % print_every == 0:
+                    print(f'\nEpoch: {epoch} \tTraining Loss: {train_loss:.4f} \tValidation Loss: {valid_loss:.4f}')
+                    print(f'\t\tTraining Accuracy: {100 * train_acc:.2f}%\t Validation Accuracy: {100 * valid_acc:.2f}%')
+
+                # Save the model if validation loss decreases
+                if valid_loss < valid_loss_min:
+                    # Save model
+                    torch.save(model.state_dict(), save_file_name)
+                    # Track improvement
+                    epochs_no_improve = 0
+                    valid_loss_min = valid_loss
+                    valid_best_acc = valid_acc
+                    best_epoch = epoch
+
+                # Otherwise increment count of epochs with no improvement
+                else:
+                    epochs_no_improve += 1
+                    # Trigger early stopping
+                    if epochs_no_improve >= max_epochs_stop:
+                        print(f'\nEarly Stopping! Total epochs: {epoch}. Best epoch: {best_epoch} with loss: {valid_loss_min:.2f} and acc: {100 * valid_acc:.2f}%')
+                        total_time = timer() - overall_start
+                        print(f'{total_time:.2f} total seconds elapsed. {total_time / (epoch+1):.2f} seconds per epoch.')
+
+                        # Load the best state dict
+                        model.load_state_dict(torch.load(save_file_name))
+                        # Attach the optimizer
+                        model.optimizer = optimizer
+
+                        # Format history
+                        history = pd.DataFrame(history, columns=['train_loss', 'valid_loss', 'train_acc', 'valid_acc'])
+                        return model, history
+
+    # Attach the optimizer
+    model.optimizer = optimizer
+    # Record overall time and print out stats
+    total_time = timer() - overall_start
+    print(f'\nBest epoch: {best_epoch} with loss: {valid_loss_min:.2f} and acc: {100 * valid_acc:.2f}%')
+    print(f'{total_time:.2f} total seconds elapsed. {total_time / (epoch):.2f} seconds per epoch.')
+    # Format history
+    history = pd.DataFrame(history, columns=['train_loss', 'valid_loss', 'train_acc', 'valid_acc'])
+    return model, history
+
+def make_predictions(model, dataloaders, train_on_gpu=True, batch_size=128):
+    
+    preds, targets = [], []
+    test_loader = dataloaders['test']
+    test_loss = 0.0
+    class_correct = list(0. for i in range(2))
+    class_total = list(0. for i in range(2))
+    classes = [0,1]
+    model.eval()
+    i=1
+    # iterate over test data
+    len(test_loader)
+    for data, target in test_loader:
+        i=i+1
+        if len(target)!=batch_size:
+            continue
+            
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        
+        # calculate the batch loss
+        loss = criterion(output, target)
+        
+        # update test loss 
+        test_loss += loss.item()*data.size(0)
+        
+        # convert output probabilities to predicted class
+        _, pred = torch.max(output, 1)
+        preds.extend(pred.cpu().numpy().tolist())
+        targets.extend(target.cpu().numpy().tolist())
+        
+        # compare predictions to true label
+        correct_tensor = pred.eq(target.data.view_as(pred))
+        correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+        
+        for i in range(batch_size):       
+            label = target.data[i]
+            class_correct[label] += correct[i].item()
+            class_total[label] += 1
+    
+    # average test loss
+    test_loss = test_loss/len(test_loader.dataset)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+    
+    eval_f1_score = f1_score(preds, targets)
+    print(f'Eval F1-Score : {eval_f1_score:.64}\n')
+    
+    for i in range(2):
+        if class_total[i] > 0:
+            print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (classes[i], 100 * class_correct[i] / class_total[i],
+                np.sum(class_correct[i]), np.sum(class_total[i])))
+        else:
+            print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+    
+    print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (100. * np.sum(class_correct) / np.sum(class_total),
+        np.sum(class_correct), np.sum(class_total)))
+
+def plot_metrics(history):
+    
+    plt.figure(figsize=(8, 6))
+    for c in ['train_loss', 'valid_loss']:
+        plt.plot(history[c], label=c)
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Average Negative Log Likelihood')
+    plt.title('Training and Validation Losses')
+
+if __name__ == '__main__':
+    
+    data_dir = './data/'
+    save_file_name = 'vgg16-chest-4.pt'
+    checkpoint_path = 'vgg16-chest-4.pth'
+    
+    dataloaders = load_pneumonia_dataset(data_dir)
+    
+    model = get_pretrained_model('vgg16')
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters())
+    
+    model, history = train(model, criterion, optimizer, dataloaders['train'], dataloaders['val'], save_file_name=save_file_name, max_epochs_stop=5, n_epochs=10, print_every=2)
+    # model, history = train(model, criterion, optimizer, dataloaders['train'], dataloaders['val'], save_file_name=save_file_name, max_epochs_stop=1, n_epochs=2, print_every=2)
+    plot_metrics(history)
+    
+    make_predictions(model, dataloaders)
